794dca86ddeb94d7cba79762d106fa933d31a6bb
byteErudite <raghuvanshivibhu@gmail.com>
2024-09-02 13:23:06 +0530

    first commit, pushing folder structure

---
+++ src/main/java/com/vaibhav/book_my_slot/BookMySlotApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.book_my_slot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMySlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMySlotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhav/book_my_slot/BookMySlotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhav.book_my_slot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMySlotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
